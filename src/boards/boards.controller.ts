c051a2ca4d001c06595f4103cc22994d7bde7d5e
soogineer <ysooroh54@gmail.com>
2023-03-14 21:23:42 +0900

    파이프를 이용한 유효성 체크

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
@@ -21,6 +23,7 @@ export class BoardsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
